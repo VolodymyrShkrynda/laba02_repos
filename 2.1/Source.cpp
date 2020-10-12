eb99b39a4b29699855ebb7f242a61300b83f66cf
VolodymyrShcrynda <volodymyr.shkrynda.itis.2020@lpnu.ua>
2020-10-12 14:37:09 +0300

    first branch_02 commit

--- 2.1/Source.cpp
+++ 2.1/Source.cpp
@@ -9,15 +9,15 @@ using namespace std;
 int main()
 {
 	double a; // вхідний параметр
-	//double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	double z1; // результат обчислення 1-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	double Pi = 4 * atan(1.); // число пі
 	cout << "a = "; cin >> a;
-	//z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
-	z2 = 2 * sqrt(2) * cos(a) * sin((Pi / 4) + (2 * a));
+	z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
+	//z2 = 2 * sqrt(2) * cos(a) * sin((Pi / 4) + (2 * a));
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
